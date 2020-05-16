cdb55636c30397415d6302890c55c96d1765eb86
jacky.yao <jacky.yao@corp.to8to.com>
2020-05-16 16:53:21 +0800

    add pimple dependency injection

--- index.php
+++ index.php
@@ -11,26 +11,33 @@ use Monolog\Processor\WebProcessor;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LogstashFormatter;
 use Monolog\Formatter\LineFormatter;
+use Pimple\Container;
 
 require "vendor/autoload.php";
+
+$container = new Container();
+$container['logger'] = function ($c) {
+    $dateFormat = 'Y-m-d$H:i:s.u';
+//$output = "%datetime% > %level_name% > %message% %context% %extra%\n";
+    $output = '[entry][ts]%datetime%[/ts][msg]%message%[/msg][context][/context][level_name]%level_name%[/level_name][lv]%level_name%[/lv][channel]%channel%[/channel][extra]%extra%[/extra][/entry]' . PHP_EOL;
+    $formatter = new LineFormatter($output, $dateFormat);
+
+
+    $logger = new Logger("zhibo");
+    $logfileName = 'prod-' . date('Y-m-d') . '.log';
+    $stream = new StreamHandler($logfileName);
+//$stream->setFormatter(new LogstashFormatter('mylog'));
+    $stream->setFormatter($formatter);
+
+    $logger->pushHandler($stream,Logger::WARNING);
+    return $logger;
+};
+
 $dotEnv = Dotenv\Dotenv::createImmutable(__DIR__, '.env');
 $dotEnv->load();
 $env = getenv('ENV');
 dump($env);
 
-$dateFormat = 'Y-m-d$H:i:s.u';
-//$output = "%datetime% > %level_name% > %message% %context% %extra%\n";
-$output = '[entry][ts]%datetime%[/ts][msg]%message%[/msg][context][/context][level_name]%level_name%[/level_name][lv]%level_name%[/lv][channel]%channel%[/channel][extra]%extra%[/extra][/entry]' . PHP_EOL;
-$formatter = new LineFormatter($output, $dateFormat);
-
-
-$logger = new Logger("zhibo");
-$logfileName = 'prod-' . date('Y-m-d') . '.log';
-$stream = new StreamHandler($logfileName);
-//$stream->setFormatter(new LogstashFormatter('mylog'));
-$stream->setFormatter($formatter);
-
-$logger->pushHandler($stream,Logger::WARNING);
 //$logger->pushProcessor(function ($data){
 //    if($data && is_array($data)){
 //        foreach ($data as $k => $val){
@@ -54,6 +61,7 @@ $logger->pushHandler($stream,Logger::WARNING);
 //    return $record;
 //});
 //$logger->pushProcessor(["Monolog\\Processor\\WebProcessor", ""]);
+$logger = $container['logger'];
 $logger->info("hello");
 $logger->warn("world");
 
